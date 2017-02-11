b3dd2fb08c8d7d3e54a587bbb58aaef08af8d613
Gustavo Barron <cicloid@42fu.com>
2017-02-11 11:31:33 -0600

    Added game?

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,16 +4,31 @@ class Game < ApplicationRecord
 
   validates :player_one_score,
     numericality: {
-      only_integer: true,
-      greater_than_or_equal_to: 0,
-      less_than_or_equal_to: 21
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 21
   }, presence: true
 
   validates :player_two_score,
     numericality: {
-      only_integer: true,
-      greater_than_or_equal_to: 0,
-      less_than_or_equal_to: 21
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 21
   }, presence: true
+
+  validate :valid_point_lead_for_win
+
+  private
+
+  # TODO: Intent should be clear on this one. probable a less convuluted way
+  # to do this exists
+  def valid_point_lead_for_win
+    return unless self.player_one_score && self.player_two_score
+    scores = [self.player_one_score, self.player_two_score].sort
+    difference = scores[1] - scores[0]
+    if difference < 2 && difference > 0
+      errors.add(:base, "Point difference should be of at least two points")
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :games, except: [:destroy, :edit, :update]
   get '/history', to: 'home#history'
-  get '/log',     to: 'home#log'
-  root to: "home#index"
+  root to: "games#index"
 end
