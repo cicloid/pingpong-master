8370fc1777690abb501709b651aeb5e912d528d5
Gustavo Barron <cicloid@42fu.com>
2017-02-11 14:04:47 -0600

    Added missing high level stories for leaderboard

--- spec/features/leaderboard_spec.rb
+++ spec/features/leaderboard_spec.rb
@@ -1,20 +1,64 @@
 require 'rails_helper'
 
 RSpec.feature "Leaderboard", type: :feature do
-  let!(:player_one) { User.create!(email: 'playerone@me.com', password: '123123123') }
-  let!(:player_two) { User.create!(email: 'playertwo@me.com', password: '123123123') }
+  let(:player_one) { User.create!(email: 'playerone@me.com', password: '123123123') }
+  let(:player_two) { User.create!(email: 'playertwo@me.com', password: '123123123') }
+
+  let(:player_one_with_one_win) { User.create!(email: 'playerone@me.com', 
+                                               password: '123123123',
+                                               rating: 1016) }
+  let(:player_two_with_one_loss) { User.create!(email: 'playertwo@me.com', 
+                                                 password: '123123123',
+                                                 rating: 985) }
 
   it "adds a new game and shows it on the user history" do
+    opponent = player_two
+    player = player_one
 
-    login_as user: player_one.email, password: "123123123"
+    login_as user: player.email, password: "123123123"
     click_link "Log Game"
     fill_in "Your Score", with: "21"
     fill_in "Their Score", with: "19"
-    select player_two.email, from: "game_player_two_id"
+    select opponent.email, from: "game_player_two_id"
     click_on "Save"
     click_on "Leaderboard"
+
     expect(page).to have_text("985")
     expect(page).to have_text("1016")
+  end
+
+
+  it "lose a game and get a lower rating" do
+    opponent = player_two_with_one_loss
+    player = player_one_with_one_win
+
+    login_as user: player.email, password: "123123123"
+    click_link "Log Game"
+
+    fill_in "Your Score", with: "18"
+    fill_in "Their Score", with: "21"
+    select opponent.email, from: "game_player_two_id"
+    click_on "Save"
+    click_on "Leaderboard"
+
+    expect(page).to have_text("1001")
+    expect(page).to have_text("999")
+  end
+
+  it "draw a game no change in score" do
+    opponent = player_two
+    player = player_one
+
+    login_as user: player.email, password: "123123123"
+    click_link "Log Game"
+    fill_in "Your Score", with: "21"
+    fill_in "Their Score", with: "21"
+    select opponent.email, from: "game_player_two_id"
+    click_on "Save"
+    click_on "Leaderboard"
+
+    expect(page).to have_text("1000")
+    expect(page).to have_text("1000")
   end
 
   def login_as(user: nil, password: nil)
